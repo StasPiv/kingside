ae43281f39ec91273569b1dea4410bff83d5b5dc
Stanislav Pivovartsev <stanislav.pivovartsev@multitude.com>
2026-04-05 11:30:37 +0200

    KS-1170: ECS 0.5 vCPU для broadcast sync

--- infra/aws/setup-task-definition.sh
+++ infra/aws/setup-task-definition.sh
@@ -45,8 +45,8 @@ aws ecs register-task-definition \
     --family kingside-api \
     --network-mode awsvpc \
     --requires-compatibilities FARGATE \
-    --cpu 256 \
-    --memory 512 \
+    --cpu 512 \
+    --memory 1024 \
     --execution-role-arn "arn:aws:iam::${ACCOUNT_ID}:role/ecsTaskExecutionRole" \
     --task-role-arn "arn:aws:iam::${ACCOUNT_ID}:role/ecsTaskRole" \
     --container-definitions "[{
